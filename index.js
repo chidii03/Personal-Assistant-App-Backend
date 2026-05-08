c4adc791a55489db728f909f8d7a9632dba60985
Vertex Vault Tech Company <stephenokwu795@gmail.com>
2026-05-08 22:57:04 +0100

    Update index.js
    cors

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://chappieai.vercel.app'
+}));
 app.use(express.json());
 
 app.use((req, res, next) => {
